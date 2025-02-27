14a548230d839b39453a9df3bd86bcd8ef5d0f82
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2025-02-28 02:41:39 +0530

    Added tiled conv2d kernel and fixed yesterday's bug

--- day19/tiled_conv2d.cu
+++ day19/tiled_conv2d.cu
@@ -4,6 +4,7 @@
 
 #define MASK_DIM 5
 #define MASK_RADIUS 2
+#define TILE_DIM 32
 
 struct timer {
   struct timespec start_time, end_time;
@@ -28,6 +29,41 @@ __constant__ float mask_c[MASK_DIM][MASK_DIM];
 
 __host__ __device__ size_t cdiv(size_t a, size_t b) { return (a + b - 1) / b; }
 
+__global__ void tiled_conv2d_kernel(float *in_data, float *out_data, size_t N,
+                                    size_t M) {
+  int32_t inRow = (blockDim.y - 2 * MASK_RADIUS) * blockIdx.y + threadIdx.y - MASK_RADIUS;
+  int32_t inCol = (blockDim.x - 2 * MASK_RADIUS) * blockIdx.x + threadIdx.x - MASK_RADIUS;
+
+  __shared__ float in_data_s[TILE_DIM][TILE_DIM];
+
+  if (inRow >= 0 && inRow < (int32_t)N && inCol >= 0 && inCol < (int32_t)M) {
+    in_data_s[threadIdx.y][threadIdx.x] = in_data[inRow * M + inCol];
+  } else {
+    in_data_s[threadIdx.y][threadIdx.x] = 0;
+  }
+
+  __syncthreads();
+
+  if (threadIdx.x >= TILE_DIM - 2 * MASK_RADIUS ||
+      threadIdx.y >= TILE_DIM - 2 * MASK_RADIUS)
+    return;
+
+  int32_t outRow = inRow + MASK_RADIUS;
+  int32_t outCol = inCol + MASK_RADIUS;
+
+  if (outRow >= N || outCol >= M)
+    return;
+
+  float sum = 0.0f;
+  for (size_t maskRow = 0; maskRow < MASK_DIM; ++maskRow) {
+    for (size_t maskCol = 0; maskCol < MASK_DIM; ++maskCol) {
+      sum += mask_c[maskRow][maskCol] *
+             in_data_s[threadIdx.y + maskRow][threadIdx.x + maskCol];
+    }
+  }
+  out_data[outRow * M + outCol] = sum;
+}
+
 __global__ void conv2d_kernel(float *in_data, float *out_data, size_t N,
                               size_t M) {
   size_t outRow = blockDim.y * blockIdx.y + threadIdx.y;
@@ -40,12 +76,39 @@ __global__ void conv2d_kernel(float *in_data, float *out_data, size_t N,
     for (size_t maskCol = 0; maskCol < MASK_DIM; ++maskCol) {
       int32_t inRow = outRow - MASK_RADIUS + maskRow;
       int32_t inCol = outCol - MASK_RADIUS + maskCol;
-      if (inRow >= 0 && inRow < (int32_t)N && inCol >= 0 && inCol < (int32_t)M) {
-        sum += mask_c[maskRow][maskCol] * in_data[inRow * M + inCol];
+      if (inRow >= 0 && inRow < (int32_t)N && inCol >= 0 &&
+          inCol < (int32_t)M) {
+        sum += in_data[inRow * M + inCol] * mask_c[maskRow][maskCol];
       }
     }
   }
   out_data[outRow * M + outCol] = sum;
+}
+
+void tiled_conv2d_gpu(float *in_data, float *out_data, float *mask, size_t N,
+                      size_t M) {
+  float *in_data_d, *out_data_d;
+
+  cudaMalloc(&in_data_d, N * M * sizeof(float));
+  cudaMalloc(&out_data_d, N * M * sizeof(float));
+
+  cudaMemcpy(in_data_d, in_data, N * M * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpyToSymbol(mask_c, mask, MASK_DIM * MASK_DIM * sizeof(float));
+
+  dim3 numThreads(32, 32);
+  dim3 numBlocks(cdiv(M, numThreads.x - MASK_RADIUS * 2),
+                 cdiv(N, numThreads.y - MASK_RADIUS * 2));
+  start_timer(&t);
+  tiled_conv2d_kernel<<<numBlocks, numThreads>>>(in_data_d, out_data_d, N, M);
+  cudaDeviceSynchronize();
+  stop_timer(&t);
+  printf("GPU time(tiled): %f\n", time_diff(&t));
+
+  cudaMemcpy(out_data, out_data_d, N * M * sizeof(float),
+             cudaMemcpyDeviceToHost);
+
+  cudaFree(in_data_d);
+  cudaFree(out_data_d);
 }
 
 void conv2d_gpu(float *in_data, float *out_data, float *mask, size_t N,
@@ -83,8 +146,11 @@ void conv2d_cpu(float *in_data, float *out_data, float *mask, size_t N,
         for (size_t maskCol = 0; maskCol < MASK_DIM; ++maskCol) {
           int32_t inRow = outRow - MASK_RADIUS + maskRow;
           int32_t inCol = outCol - MASK_RADIUS + maskCol;
-          if (inRow >= 0 && inRow < (int32_t)N && inCol >= 0 && inCol < (int32_t)M)
-            sum += in_data[inRow * M + inCol] * mask[maskRow * MASK_DIM + maskCol];
+          if (inRow >= 0 && inRow < (int32_t)N && inCol >= 0 &&
+              inCol < (int32_t)M) {
+            sum +=
+                in_data[inRow * M + inCol] * mask[maskRow * MASK_DIM + maskCol];
+          }
         }
       }
       out_data[outRow * M + outCol] = sum;
@@ -98,8 +164,8 @@ bool allclose(float *A, float *B, size_t N, size_t M) {
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < M; ++j) {
       if (abs(A[i * M + j] - B[i * M + j]) > 1e-4) {
-        printf("Mismatch at (%lu, %lu), A = %f and B = %f", i, j, A[i * M + j],
-               B[i * M + j]);
+        printf("Mismatch at (%lu, %lu), A = %f and B = %f\n", i, j,
+               A[i * M + j], B[i * M + j]);
         return false;
       }
     }
@@ -110,13 +176,14 @@ bool allclose(float *A, float *B, size_t N, size_t M) {
 int main() {
   cudaDeviceSynchronize();
 
-  float *in_data, *out_data_cpu, *out_data_gpu, *mask;
+  float *in_data, *out_data_cpu, *out_data_gpu, *out_data_gpu_tiled, *mask;
   size_t N, M;
 
-  N = 2048, M = 2048;
+  N = 8192, M = 8192;
   in_data = (float *)malloc(N * M * sizeof(float));
   out_data_cpu = (float *)malloc(N * M * sizeof(float));
   out_data_gpu = (float *)malloc(N * M * sizeof(float));
+  out_data_gpu_tiled = (float *)malloc(N * M * sizeof(float));
   mask = (float *)malloc(MASK_DIM * MASK_DIM * sizeof(float));
 
   for (size_t i = 0; i < N; ++i) {
@@ -135,8 +202,13 @@ int main() {
 
   conv2d_gpu(in_data, out_data_gpu, mask, N, M);
 
-  printf("GPU and CPU impl match: %s",
+  tiled_conv2d_gpu(in_data, out_data_gpu_tiled, mask, N, M);
+
+  printf("GPU and CPU impl match: %s\n",
          allclose(out_data_cpu, out_data_gpu, N, M) ? "true" : "false");
+
+  printf("GPU and CPU impl match: %s\n",
+         allclose(out_data_cpu, out_data_gpu_tiled, N, M) ? "true" : "false");
 
   free(in_data);
   free(out_data_cpu);
