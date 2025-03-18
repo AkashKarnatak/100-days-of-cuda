eab5ca6557506cc25c92b3a44ba39aa1212e2b63
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2025-03-18 19:34:32 +0530

    Started coding flash attention

---
+++ day33/flash_attention.cu
@@ -0,0 +1,6 @@
+__global__ void flash_attention_kernel(float *query, float *key, float *value, size_t N, size_t d) {
+
+}
+
+void flash_attention_gpu(float *query, float *key, float *value, size_t N, size_t d) {
+}
