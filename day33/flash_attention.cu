77f115eecf2fb10c846b8811f3ee10f2baf134ca
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2025-03-19 15:40:57 +0530

    Optimized shared mem

--- day33/flash_attention.cu
+++ day33/flash_attention.cu
@@ -7,16 +7,13 @@ __global__ void flash_attention_kernel(float *query, float *key, float *value,
                                        size_t N, size_t d, size_t Br, size_t Bc,
                                        size_t Tr, size_t Tc) {
   size_t tid = threadIdx.x;
-  float *Q, *K, *V, *O, *S, *m, *l;
+  float *Q, *K, *V, *S;
 
-  __shared__ float sram[11 * 1024];
-  Q = &sram[0];                                // Br x d
-  K = &sram[Br * d];                           // Bc x d
-  V = &sram[Br * d + Bc * d];                  // Bc x d
-  O = &sram[Br * d + 2 * Bc * d];              // Br x d
-  l = &sram[2 * Br * d + 2 * Bc * d];          // Br
-  m = &sram[2 * Br * d + 2 * Bc * d + Br];     // Br
-  S = &sram[2 * Br * d + 2 * Bc * d + 2 * Br]; // Br x d
+  extern __shared__ float sram[];
+  Q = &sram[0];                   // Br x d
+  K = &sram[Br * d];              // Bc x d
+  V = &sram[Br * d + Bc * d];     // Bc x d
+  S = &sram[Br * d + 2 * Bc * d]; // Br x Bc
 
   for (size_t j = 0; j < Tc; ++j) {
     __syncthreads();
@@ -35,23 +32,14 @@ __global__ void flash_attention_kernel(float *query, float *key, float *value,
     __syncthreads();
 
     for (size_t i = 0; i < Tr; ++i) {
-      // load Q, O, m, l
+      // load Q
       if (tid < Br) {
         for (size_t k = 0; k < d; ++k) {
           if (i * Br + tid < N) {
             Q[tid * d + k] = query[(i * Br + tid) * d + k];
-            O[tid * d + k] = output[(i * Br + tid) * d + k];
           } else {
             Q[tid * d + k] = -INFINITY;
-            O[tid * d + k] = 0;
           }
-        }
-        if (i * Br + tid < N) {
-          l[tid] = sums[i * Br + tid];
-          m[tid] = maxes[i * Br + tid];
-        } else {
-          l[tid] = 0;
-          m[tid] = -INFINITY;
         }
       }
 
@@ -60,8 +48,10 @@ __global__ void flash_attention_kernel(float *query, float *key, float *value,
       if (tid >= Br || i * Br + tid >= N)
         continue;
 
-      float m_, l_, m_new, l_new;
-      m_ = -INFINITY, l_ = 0;
+      float m_prev, l_prev, m_curr, l_curr, m_new, l_new;
+
+      m_prev = maxes[i * Br + tid], l_prev = sums[i * Br + tid];
+      m_curr = -INFINITY, l_curr = 0;
 
       for (size_t col = 0; col < Bc; ++col) {
         float sum = 0.0f;
@@ -69,16 +59,16 @@ __global__ void flash_attention_kernel(float *query, float *key, float *value,
           sum += Q[tid * d + k] * K[col * d + k];
         }
         S[tid * Bc + col] = sum / sqrtf(d);
-        m_ = fmaxf(m_, sum / sqrtf(d));
+        m_curr = fmaxf(m_curr, sum / sqrtf(d));
       }
 
       for (size_t col = 0; col < Bc; ++col) {
-        S[tid * Bc + col] = expf(S[tid * Bc + col] - m_);
-        l_ += S[tid * Bc + col];
+        S[tid * Bc + col] = expf(S[tid * Bc + col] - m_curr);
+        l_curr += S[tid * Bc + col];
       }
 
-      m_new = max(m[tid], m_);
-      l_new = l[tid] * expf(m[tid] - m_new) + l_ * expf(m_ - m_new);
+      m_new = max(m_prev, m_curr);
+      l_new = l_prev * expf(m_prev - m_new) + l_curr * expf(m_curr - m_new);
 
       for (size_t col = 0; col < d; ++col) {
         float sum = 0.0f;
@@ -86,8 +76,8 @@ __global__ void flash_attention_kernel(float *query, float *key, float *value,
           sum += S[tid * Bc + k] * V[k * d + col];
         }
         output[(i * Br + tid) * d + col] =
-            (l[tid] * O[tid * d + col] * expf(m[tid] - m_new) +
-             sum * expf(m_ - m_new)) /
+            (l_prev * output[(i * Br + tid) * d + col] * expf(m_prev - m_new) +
+             sum * expf(m_curr - m_new)) /
             l_new;
       }
       sums[i * Br + tid] = l_new;
@@ -102,7 +92,7 @@ void flash_attention_gpu(float *query, float *key, float *value, float *output,
   size_t Br, Bc, Tr, Tc;
   float *query_d, *key_d, *value_d, *output_d, *sums_d, *maxes_d;
 
-  Br = 16, Bc = 16;
+  Br = 24, Bc = 24;
   Tr = cdiv(N, Br), Tc = cdiv(N, Bc);
 
   cudaMalloc(&query_d, (N * d) * sizeof(float));
@@ -121,8 +111,18 @@ void flash_attention_gpu(float *query, float *key, float *value, float *output,
 
   dim3 numThreads(Bc);
   dim3 numBlocks(1);
-  flash_attention_kernel<<<numBlocks, numThreads>>>(
+  size_t sramMem = (Br * d    // Q
+                    + Bc * d  // K
+                    + Bc * d  // V
+                    + Br * Bc // S
+                    )
+                   * sizeof(float);
+  flash_attention_kernel<<<numBlocks, numThreads, sramMem>>>(
       query_d, key_d, value_d, output_d, sums_d, maxes_d, N, d, Br, Bc, Tr, Tc);
+  cudaError_t err = cudaGetLastError(); // Check for launch errors
+  if (err != cudaSuccess) {
+    printf("CUDA Error: %s\n", cudaGetErrorString(err));
+  }
   cudaDeviceSynchronize();
 
   cudaMemcpy(output, output_d, (N * d) * sizeof(float), cudaMemcpyDeviceToHost);
@@ -166,10 +166,6 @@ int main() {
   }
 
   flash_attention_gpu(query, key, value, output, sums, maxes, N, d);
-  cudaError_t err = cudaGetLastError(); // Check for launch errors
-  if (err != cudaSuccess) {
-    printf("CUDA Error: %s\n", cudaGetErrorString(err));
-  }
 
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < d; ++j) {
